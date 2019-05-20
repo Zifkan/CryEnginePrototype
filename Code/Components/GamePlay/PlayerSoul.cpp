46191ffbeebcd84583aa535c18b3a043def17524
Ylly <zifkan@mail.ru>
2019-05-20 23:08:41 +0300

    Added Player Soul component

---
+++ Code/Components/GamePlay/PlayerSoul.cpp
@@ -0,0 +1,63 @@
+﻿#include "StdAfx.h"
+
+#include <CrySchematyc/Env/IEnvRegistrar.h>
+#include <CrySchematyc/Env/Elements/EnvComponent.h>
+#include "PlayerSoul.h"
+
+static void RegisterCPlayerSoul(Schematyc::IEnvRegistrar& registrar)
+{
+    Schematyc::CEnvRegistrationScope scope = registrar.Scope(IEntity::GetEntityScopeGUID());
+    {
+        Schematyc::CEnvRegistrationScope componentScope = scope.Register(SCHEMATYC_MAKE_ENV_COMPONENT(CPlayerSoul));
+        // Functions
+        {
+        }
+    }
+}
+
+CRY_STATIC_AUTO_REGISTER_FUNCTION(&RegisterCPlayerSoul)
+
+void CPlayerSoul::Initialize()
+{
+    m_lifeTimeLimit = 10.f;
+    m_pPoinLight = m_pEntity->GetOrCreateComponent<Cry::DefaultComponents::CPointLightComponent>();
+}
+
+Cry::Entity::EntityEventMask CPlayerSoul::GetEventMask() const
+{
+    return ENTITY_EVENT_BIT(ENTITY_EVENT_START_GAME) | ENTITY_EVENT_BIT(ENTITY_EVENT_UPDATE) | ENTITY_EVENT_BIT(ENTITY_EVENT_EDITOR_PROPERTY_CHANGED);
+}
+
+void CPlayerSoul::ProcessEvent(const SEntityEvent& event)
+{
+    switch (event.event)
+    {
+    case ENTITY_EVENT_START_GAME:
+    {
+        LifeTimeReaction();
+    }
+    break;
+    case ENTITY_EVENT_UPDATE:
+    {
+        SEntityUpdateContext* pCtx = (SEntityUpdateContext*)event.nParam[0];
+        m_lifeTimeCycle.get_subscriber().on_next(pCtx->fFrameTime);
+    }
+    break;
+    case ENTITY_EVENT_COMPONENT_PROPERTY_CHANGED:
+    {
+      
+    }
+    break;
+    }
+}
+
+void CPlayerSoul::LifeTimeReaction()
+{
+    m_lifeTimeCycle.get_observable().scan(m_lifeTimeLimit,[](float v, float time){return v - time;})
+    .take_while([](float v) {return v > 0; })
+    .subscribe([this](float time)
+    {
+      /  m_pPoinLight->GetColorParameters()
+    });
+
+}
---
+++ Code/Components/GamePlay/PlayerSoul.h
@@ -0,0 +1,34 @@
+﻿#pragma once
+#include <CryEntitySystem/IEntitySystem.h>
+#include <CryEntitySystem/IEntityComponent.h>
+#include <DefaultComponents/Lights/PointLightComponent.h>
+#include "RxCpp/rx.hpp"
+
+
+class CPlayerSoul : public IEntityComponent
+{
+public:
+    ~CPlayerSoul(){}
+
+    static void ReflectType(Schematyc::CTypeDesc<CPlayerSoul>& desc)
+    {
+        desc.SetGUID("{D4CC9077-1852-4467-B060-6C5A1EB7B671}"_cry_guid);
+        desc.SetEditorCategory("Game Play");
+        desc.SetLabel("Player Soul Component");
+        
+    }
+
+
+    Cry::Entity::EntityEventMask GetEventMask() const override;
+
+protected:
+    void Initialize() override;
+    void ProcessEvent(const SEntityEvent& event) override;
+
+    Cry::DefaultComponents::CPointLightComponent* m_pPoinLight = nullptr;
+
+    void LifeTimeReaction();
+
+    float m_lifeTimeLimit = 0.0f;
+    rxcpp::subjects::subject<float> m_lifeTimeCycle = rxcpp::subjects::subject<float>();
+};
